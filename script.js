e6529c8e046d9c9a7c252cb3fcd80e0f83faa6d4
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-03-19 15:21:10 -0400

    Added loadQuestion

--- script.js
+++ script.js
@@ -46,11 +46,66 @@ quiz.addQuestion(new Question("What geometric shape is generally used for stop s
 	"Hexagon": false
 }))
 
-// --------------------------------------------------------
+/** @type {Array} questions in quiz */
+const questions = quiz.getQuestions()
 
+// --------------------------------------------------------
+/**
+ * Adds the question and options to the form
+ * @param {Question} question The question
+ */
+function loadQuestion(question)
+{
+	// sets up the asked question
+	const questionHeader = document.querySelector("#question")
+
+	if (!questionHeader) return false
+
+	questionHeader.innerHTML = `${quiz.getQuestionNumber(question)}) `
+	questionHeader.innerHTML += question.getQuestion()
+
+	// sets up the options
+	/** @type {Object} options object */
+	const optionsObj = question.getOptions()
+
+	/** @type {Array} keys to the options object */
+	const keysArray = Object.keys(optionsObj)
+
+	// creates each option to be displayed
+	for (let i = 0; i < keysArray.length; i++)
+	{
+		builder.start(document.querySelector("#options"))
+		{
+			builder.startElement("div")
+			{
+				builder.addClass("option_div")
+
+				// radio button
+				builder.startElement("input")
+				{
+					builder.setAttribute("type", "radio")
+					builder.setAttribute("id", String(i))
+					builder.setAttribute("value", keysArray[i])
+					builder.setAttribute("name", "options")
+				}
+				builder.endElement()
+
+				// label
+				builder.startElement("label")
+				{
+					builder.setAttribute("for", String(i))
+					builder.setProperty("innerHTML", keysArray[i])
+				}
+				builder.endElement()
+			}
+			builder.endElement()
+		}
+		builder.end()
+	}
+}
 
 /////////////////////////////////////////////////////
 helper.hookEvent(window, "load", false, () =>
 {
-
+	loadQuestion(questions[0])
 })
